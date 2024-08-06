a363b99bae46c13f6a6945f947732343c89121c0
Abin-Y <abinxnxx123@gmail.com>
2024-08-06 09:35:29 +0545

    Add files via upload

---
+++ server.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const session = require("express-session");
+const nocache = require("nocache");
+const app = express();
+
+const userName = "abin@gmail.com";
+const passWord = "abin129";
+
+app.use(nocache());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "my-secret-key",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.set("view engine", "ejs");
+app.use(express.static("views"));
+
+app.get("/", (req, res) => {
+  if (req.session.user) {
+    res.render("dashboard", {username : req.session.user});
+  } else if (req.session.pass) {
+    req.session.pass = false;
+    const msg = 'Invalid Credentials'
+    res.render("login", {errMsg : msg});
+  } else {
+    const msg = ''
+    res.render("login", {errMsg : msg});
+  }
+});
+
+app.post("/verify", (req, res) => {
+  if (req.body.email === userName && req.body.password === passWord) {
+    req.session.user = userName;
+    res.redirect("/");
+  } else {
+    req.session.pass = true;
+    res.redirect("/");
+  }
+});
+
+app.get("/login", (req, res) => {
+  res.redirect("/");
+});
+
+app.get("/dashboard", (req, res) => {
+  res.redirect("/");
+});
+
+app.post("/logout", (req, res) => {
+  req.session.user = undefined;
+  res.redirect("/");
+});
+
+app.get('/homenew', (req,res) => {
+  res.render('home')
+})
+
+// app.get('/acct', (req,res)  //   res.render('newaccout')  This is the 
+// })
+
+app.get('/newpro', (req,res) => {
+  res.render('profile')
+})
+
+app.get("*", (req, res) => {
+  res.render("404");
+});
+
+const PORT = 3002;
+app.listen(PORT, (err) => {
+  if (err) {
+    throw err
+  }
+    console.log(`server is running on port`, PORT)
+});
